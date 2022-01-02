d33948d3b145592674cccee954aa35fce66586ae
batuu12 <57187074+batuu12@users.noreply.github.com>
2022-01-02 15:18:16 +0300

    first commit

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+
+        public void Delete(Category entity)
+        {
+            c.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
