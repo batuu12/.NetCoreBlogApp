aca5bc4692a49c6086beee6f2043e81f4e4c334c
batuu12 <57187074+batuu12@users.noreply.github.com>
2022-01-17 02:05:55 +0300

    -ViewComponent eklendi.

---
+++ CoreBlogApp/ViewComponents/CommentList.cs
@@ -0,0 +1,30 @@
+﻿using CoreBlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogApp.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id=1,
+                    UserName="Batuhan"
+                },
+                new UserComment
+                {
+                    Id=2,
+                    UserName="Murat"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
